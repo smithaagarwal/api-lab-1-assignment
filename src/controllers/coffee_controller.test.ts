31acee4a7492ac9f997f7b21ccf61168a5b89522
Smitha <smitha.kotian@gmail.com>
2023-11-22 08:20:20 +0000

    feat: Implement GET request for coffee endpoint and test

---
+++ src/controllers/coffee_controller.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+import { app } from "../app";
+
+describe("Test /coffee endpoint request", () => {
+  test("GET /coffee should return correct object", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Latte" });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
+  });
+});
